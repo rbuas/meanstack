41b80a19fc17745a9b0007330a94ae613c465d35
RodrigoBuas <rodrigobuas@gmail.com>
2016-07-10 17:50:02 +0200

    autotest

---
+++ tests/basicload.js
@@ -0,0 +1,13 @@
+var _expect = require("chai").expect;
+var _assert = require("chai").assert;
+
+describe("basicload", function() {
+    it("loadpage", function(){
+
+    });
+
+    it("checkdata", function() {
+        var result = "a";
+        _expect(result).to.equal("b");
+    });
+});
