fae276dffa37915bf22489ceed46c5885b1d60af
Rodrigo Buas <rodrigobuas@gmail.com>
2016-09-26 16:25:13 +0200

    test user unit

--- tests/user.unit.js
+++ tests/user.unit.js
@@ -11,7 +11,40 @@ describe("user-unit-create", function() {
     before(function() {
     });
 
-    it("check empty", function(done) {
+    it("create-user-null", function(done) {
+        var user =  null;
+        User.Create(user, function(err, savedUser) {
+            _expect(err).to.not.equal(null);
+            _expect(err.code).to.equal("USER_PARAMS");
+            _expect(savedUser).to.equal(false);
+            done();
+        });
+    });
+    it("create-user-empty", function(done) {
+        var user =  {};
+        User.Create(user, function(err, savedUser) {
+            _expect(err).to.not.equal(null);
+            _expect(err.code).to.equal("USER_PARAMS");
+            _expect(savedUser).to.equal(false);
+            done();
+        });
+    });
+    it("create-user-nopassword", function(done) {
+        var user =  {email:"rodrigobuas+unittest@gmail.com"};
+        User.Create(user, function(err, savedUser) {
+            _expect(err).to.not.equal(null);
+            _expect(err.code).to.equal("USER_PARAMS");
+            _expect(savedUser).to.equal(false);
+            done();
+        });
+    });
+    it("create-user-success", function(done) {
+        var user =  {email:"rodrigobuas+unittest@gmail.com", password:"123456"};
+        User.Create(user, function(err, savedUser) {
+            _expect(err).to.equal(null);
+            _expect(savedUser).to.equal(true);
+            done();
+        });
     });
 });
 
